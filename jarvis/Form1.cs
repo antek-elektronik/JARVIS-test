1cb81fd19e5c6e0cd0beee0a4cc23456f800f224
arin <67313390+6Arin9@users.noreply.github.com>
2021-08-05 16:33:55 +0200

    Remove debug code

--- jarvis/Form1.cs
+++ jarvis/Form1.cs
@@ -383,54 +383,6 @@ namespace speech_recognition_test_2
             Sre.RecognizeAsyncStop();
         }
 
-        /*
-                private void test() //skopiowane z dokumentacji, tylko do debugowania!
-                {
-                    using (SpeechSynthesizer synth = new SpeechSynthesizer())
-                    {
-
-                        // Get information about supported audio formats.  
-                        string AudioFormats = "";
-                        foreach (SpeechAudioFormatInfo fmt in synth.Voice.SupportedAudioFormats)
-                        {
-                            AudioFormats += String.Format("{0}\n",
-                            fmt.EncodingFormat.ToString());
-                        }
-
-                        // Write information about the voice to the console.  
-                        Console.WriteLine(" Name:          " + synth.Voice.Name);
-                        Console.WriteLine(" Culture:       " + synth.Voice.Culture);
-                        Console.WriteLine(" Age:           " + synth.Voice.Age);
-                        Console.WriteLine(" Gender:        " + synth.Voice.Gender);
-                        Console.WriteLine(" Description:   " + synth.Voice.Description);
-                        Console.WriteLine(" ID:            " + synth.Voice.Id);
-                        if (synth.Voice.SupportedAudioFormats.Count != 0)
-                        {
-                            Console.WriteLine(" Audio formats: " + AudioFormats);
-                        }
-                        else
-                        {
-                            Console.WriteLine(" No supported audio formats found");
-                        }
-
-                        // Get additional information about the voice.  
-                        string AdditionalInfo = "";
-                        foreach (string key in synth.Voice.AdditionalInfo.Keys)
-                        {
-                            AdditionalInfo += String.Format("  {0}: {1}\n",
-                              key, synth.Voice.AdditionalInfo[key]);
-                        }
-
-                        Console.WriteLine(" Additional Info - " + AdditionalInfo);
-                        Console.WriteLine();
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine("Press any key to exit...");
-                    Console.Read();
-                  }
-        */
-
         //auto scroll
         private void timer1_Tick(object sender, EventArgs e)
         {
